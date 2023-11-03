24eb77c888426a85b679a85569c73942e98c8150
Goonber <gunnar.oscarson1@gmail.com>
2023-11-03 16:53:37 +0100

    build changes

--- app/src/main/java/com/example/notesapp/NoteItem.kt
+++ app/src/main/java/com/example/notesapp/NoteItem.kt
@@ -4,11 +4,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-
-
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun NoteItem(
@@ -24,9 +23,9 @@ fun NoteItem(
         Column(
             modifier = Modifier.padding(16.dp)
         ) {
-            Text(text = note.title, style = MaterialTheme.typography.h6)
+            Text(text = note.title, fontSize = 16.sp, fontWeight = FontWeight.Normal)
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = note.text, style = MaterialTheme.typography.body2)
+            Text(text = note.text, fontSize = 14.sp, fontWeight = FontWeight.Normal)
         }
     }
 }
